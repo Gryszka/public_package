69420208293dab5adc4e62c4eeec8f26fe8338b1
Gryszka <romanowicz.g@gmail.com>
2024-08-23 14:18:23 +0200

    corrections 1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='gryszkapackage',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[],
-    author='Twoje Imię',
-    author_email='psiowy@gmail.com',
+    author='Gryszka',
+    author_email='romanowicz.g@gmail.com',
     description='test test test',
-    url= 'https://github.com/Gryszka/test_public'
+    url= 'https://github.com/Gryszka/public_package'
 )
