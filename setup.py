02a89177a153bdbcb28d8a074932340cd324afd6
Gryszka <romanowicz.g@gmail.com>
2024-08-23 14:05:25 +0200

    part 1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Twoje Imię',
+    author_email='psiowy@gmail.com',
+    description='test test test',
+    url= 'https://github.com/Gryszka/test_public'
+)
